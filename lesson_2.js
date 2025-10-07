0ddd0d4ebc4a6b8e6b3194112dae039a66008c24
Sasha Kozel <sasha.kozel252000@gmail.com>
2025-10-07 20:14:14 +0200

    birth date added

--- lesson_2.js
+++ lesson_2.js
@@ -1,5 +1,10 @@
 const myName = "Sasha";
+const birthDate = "12.11.2000";
 
 if(myName.includes('Sasha')){
     console.log('All good!');
+}
+
+if(birthDate === '12.11.2000'){
+    console.log('date of birth is 12 Nov 2000');
 }
