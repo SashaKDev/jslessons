f7ee96721955384d947ad2fd7a2637b94608e200
Sasha Kozel <sasha.kozel252000@gmail.com>
2025-10-27 13:55:29 +0100

    .gitignore updated, codewars_10 added

---
+++ codewars_10.js
@@ -0,0 +1,20 @@
+const array = null;
+
+function countPositivesSumNegatives(input) {
+    let sum = 0;
+    let count = 0;
+    if (input === null || input.length === 0) {
+        return [];
+    } else {
+        input.forEach(el => {
+            if (el > 0) {
+                count++;
+            } else if (el < 0) {
+                sum += el;
+            }
+        })
+        return [count, sum];
+    }
+}
+
+console.log(countPositivesSumNegatives(array));
