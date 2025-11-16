b7cf457634656f4b76786bf2163c5d41523aae53
Alexander <sasha.kozel252000@gmail.com>
2025-11-16 16:44:40 +0300

    commit

--- test.js
+++ test.js
@@ -1,30 +1,4 @@
-// const names = ['Alex', 'Bob', 'Ivan', 'Sasha']
-// const reversed = [...names];
-//
-// console.log('names', names) // reverse мутировал исходный массив и мы
-// // получили: [ 'Sasha', 'Ivan', 'Bob', 'Alex' ]
-// console.log('reversedArr', reversed) //[ 'Sasha', 'Ivan', 'Bob', 'Alex' ]
+const arr = [1, 2, 3, 4, 5]
+const res = arr.includes(2)
 
-// console.log(names === reversed);
-//----------------------------------------------------------------------
-//reverse phone number
-
-// const number = '1145286';
-//
-// const numberArr = number.split('');
-//
-// const reversedNumberArr = numberArr.reverse();
-//
-// const reversedNumber = reversedNumberArr.join('');
-//
-// console.log(reversedNumber);
-//----------------------------------------------------------------------
-//concat method
-
-const arr = [1, 2, 3];
-const arr2 = [[4,5], [6,7]];
-const arr3 = [[[8,9]]]
-
-const result = arr.concat(arr2, arr3).flat(Infinity);
-
-console.log(result);
+console.log(res) //true
