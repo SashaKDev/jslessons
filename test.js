5cdc3b797b6dda2698accc0243f3efd66b0714e1
Alexander <sasha.kozel252000@gmail.com>
2025-11-16 16:22:04 +0300

    commit

--- test.js
+++ test.js
@@ -1,8 +1,20 @@
-const names = ['Alex', 'Bob', 'Ivan', 'Sasha']
-const reversed = names.reverse()
+// const names = ['Alex', 'Bob', 'Ivan', 'Sasha']
+// const reversed = [...names];
+//
+// console.log('names', names) // reverse мутировал исходный массив и мы
+// // получили: [ 'Sasha', 'Ivan', 'Bob', 'Alex' ]
+// console.log('reversedArr', reversed) //[ 'Sasha', 'Ivan', 'Bob', 'Alex' ]
+//
+// console.log(names === reversed);
 
-console.log('names', names) // reverse мутировал исходный массив и мы
-// получили: [ 'Sasha', 'Ivan', 'Bob', 'Alex' ]
-console.log('reversedArr', reversed) //[ 'Sasha', 'Ivan', 'Bob', 'Alex' ]
+//reverse phone number
 
-console.log(names === reversed);
+const number = '1145286';
+
+const numberArr = number.split('');
+
+const reversedNumberArr = numberArr.reverse();
+
+const reversedNumber = reversedNumberArr.join('');
+
+console.log(reversedNumber);
