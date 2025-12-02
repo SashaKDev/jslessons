0fc576b957b10611026695c768f7a60c222d9d92
Alexander <sasha.kozel252000@gmail.com>
2025-12-02 21:59:06 +0300

    new task in test

--- test.js
+++ test.js
@@ -2,6 +2,29 @@
 //     return number % 2 !== 0 ? 'even' : 'odd';
 // }
 
-function multiply(a, b){
-    return a * b
-}
+// function multiply(a, b){
+//     return a * b
+// }
+
+// function delay(ms) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(resolve, ms)
+//     })
+// }
+//
+// delay(3000).then(() => console.log('выполнилось через 3 секунды'));
+
+// const ob = {
+//     // id: 1,
+//     // name: 'John',
+// }
+//
+// console.log(ob.id + " " + ob.name);
+// const date1 = new Date();
+// const date2 = new Date();
+// console.log(date1 + " " + date2);
+
+const arr = [1, 2, 3, 3, 4, 'hello'];
+const set = new Set(arr);
+console.log(set);
+
