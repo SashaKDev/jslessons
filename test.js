197892634705993d64345562d2a85331736ceebe
Alexander <sasha.kozel252000@gmail.com>
2025-12-10 20:54:11 +0300

    new task in test

--- test.js
+++ test.js
@@ -24,7 +24,87 @@
 // const date2 = new Date();
 // console.log(date1 + " " + date2);
 
-const arr = [1, 2, 3, 3, 4, 'hello'];
-const set = new Set(arr);
-console.log(set);
+// const arr = [1, 2, 3, 3, 4, 'hello'];
+// const set = new Set(arr);
+// console.log(set);
+
+//globalLE {} --> null
+
+// startEngine()
+//
+// let car = 'bmw' //globalLE {car: 'bmw'} --> null
+//
+// function startEngine () {
+//     //startEngineLE {} --> globalLE
+//     const car = 'kia' // startEngineLE {car: 'kia'} --> globalLE
+//     console.log(`Start ${car}`)
+// }
+//
+// startEngine() //globalLE {startEngine: function} --> null
+//
+// car = 'audi' //globalLE {startEngine: function, car: 'audi'} --> null
+//
+// startEngine()
+
+// const OuterFunc = () => {
+//     let count = 0;
+//     return () => {console.log(count++)}
+// }
+//
+// const counter1 = OuterFunc();
+//
+// counter1();
+// counter1();
+// counter1();
+
+// function outer() {
+//     let message = "Hello!";
+//     return function inner() {
+//         console.log(message);
+//     };
+// }
+//
+// function executor(fn) {
+//     fn();
+// }
+//
+// const closureFunc = outer();
+// executor(closureFunc);
+
+const sqr = (num) => {
+    console.log('call');
+    return num * num;
+}
+
+const sum = (a, b) => {
+    console.log('call');
+    return a + b;
+}
+
+
+
+const memoize = (fn) => {
+    const cash = {};
+    return (...args) => {
+
+        if (cash[args.toString()]){
+            return cash[args.toString()];
+        } else {
+            return cash[args.toString()] = fn(...args);
+
+        }
+    }
+}
+
+const fastSqr = memoize(sqr)
+const fastSum = memoize(sum)
+
+console.log(fastSum(1, 2));
+console.log(fastSum(1, 2));
+
+console.log(fastSqr(5));
+console.log(fastSqr(5));
+console.log(fastSqr(6));
+console.log(fastSqr(7));
+console.log(fastSqr(6));
 
